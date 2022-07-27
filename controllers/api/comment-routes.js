f707ec3c4b84576068ab7e39a3dec49e663fa39f
agvan <agvandyke25@gmail.com>
2022-07-27 14:29:43 -0500

    added a POST route to create a new comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,3 +3,27 @@ const { Comment } = require("../../models");
 
 
 //GET route for all comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//POST route for creating a new comment
+router.post('/', (req, res) => {
+    Comment.create({
+        body: req.body.body,
+        post_id: req.body.post_id,
+        user_id: req.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+})
+
+module.exports = router;
