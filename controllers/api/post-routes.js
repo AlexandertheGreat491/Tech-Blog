df321ea940b2648393ab86f0a048e276afc06592
agvan <agvandyke25@gmail.com>
2022-07-27 13:27:26 -0500

    created GET route for all posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,31 @@
-const router = require('express').Router();
-const {Post, User, Comment} = require('../../models');
-const sequelize = require('../../config/connection');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+
+//post route to get all users
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "title", "body", "user_id"],
+    order: [["DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "body", "user_id", "post_id"],
+        include: {
+          model: User,
+          attributes: ["id", "username", "email", "password"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["id", "username", "email", "password"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
