9b2c401b0cbba9db1c351276bed879c963b48a25
agvan <agvandyke25@gmail.com>
2022-07-27 10:53:02 -0500

    created POST route for posting/adding new users to the user table in the database

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,7 +49,18 @@ router.get("/:id", (req, res) => {
 });
 
 //POST /api/users
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 //PUT /api/users
 router.put("/:id", (req, res) => {});
