bf81266786630763e3f156f9336c07cc1c81e941
agvan <agvandyke25@gmail.com>
2022-07-27 10:34:26 -0500

    updated route for GET all users to exclude password & created route to get user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,26 +1,60 @@
-const router = require('express').Router();
-const {User, Post, Comment} = require('../../models');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
 
 //GET /api/users
-router.get('/', (req, res) => {
-    User.findAll()
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 //GET /api/users/id
-router.get('/:id', (req, res) => {});
+router.get("/:id", (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "body", "user_id"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "body", "user_id", "post_id"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 //POST /api/users
-router.post('/', (req, res) => {});
+router.post("/", (req, res) => {});
 
 //PUT /api/users
-router.put('/:id', (req, res) => {});
+router.put("/:id", (req, res) => {});
 
 //DELETE /api/users
-router.delete('/:id', (req, res) => {});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
