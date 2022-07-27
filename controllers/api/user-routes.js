86879f70b5a0fec6d850a04f9c49dc1b8e06acb6
agvan <agvandyke25@gmail.com>
2022-07-27 10:23:02 -0500

    created route to get all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,10 +3,12 @@ const {User, Post, Comment} = require('../../models');
 
 //GET /api/users
 router.get('/', (req, res) => {
-    User.findAll({
-        attributes: {exclude: ['password']}
-    })
-    
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //GET /api/users/id
