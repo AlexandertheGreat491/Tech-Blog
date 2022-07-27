f94c65d3829550ae0eaff7417babcac05030d4c8
agvan <agvandyke25@gmail.com>
2022-07-27 13:52:27 -0500

    created PUT route to update posts by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -76,3 +76,27 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//PUT route to update a post by id
+router.put('/:id', (req, res) => {
+    Post.update({
+        title: req.body.title,
+        body: req.body.body
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'No post found with this id!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
