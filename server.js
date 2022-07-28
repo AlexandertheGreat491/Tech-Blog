cbd10d8385bfb1f5f03e43cade2b818d460114bc
agvan <agvandyke25@gmail.com>
2022-07-28 14:45:37 -0500

    changed the logic

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 app.listen(PORT, () => {
