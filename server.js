3dfe803024ab9063277d37c8f60ce475e9e2b93f
agvan <agvandyke25@gmail.com>
2022-07-28 15:04:47 -0500

    set the session to automatically expire after 10 minutes

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    //user's session will automatically expire after 10 minutes
+    expires: 10 * 60 * 1000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
