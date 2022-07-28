0841dbb52ba7e78f7321b753da879cac73434b7a
agvan <agvandyke25@gmail.com>
2022-07-28 12:30:25 -0500

    created the signupFormHandler() function to handle the signup form

---
+++ public/javascript/login.js
@@ -0,0 +1,29 @@
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    //checks the response status when this route is accessed
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
