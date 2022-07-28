dd57a2876893f4919580065f68201159da91819c
agvan <agvandyke25@gmail.com>
2022-07-28 10:40:42 -0500

    imported sequelize & all 3 models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User, Comment} = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {
+        id: 1,
+        body: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebar Docs',
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
 });
 
 module.exports = router;
