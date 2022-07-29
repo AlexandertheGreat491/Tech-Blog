92513fa983b84001e0555a531243a619e930bf6b
agvan <agvandyke25@gmail.com>
2022-07-28 23:22:27 -0500

    protected routes with middleware & created the edit-post.handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
+
 //route to get all posts
 router.get("/", (req, res) => {
   console.log("======================");
